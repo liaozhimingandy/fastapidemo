702528f45ccf59c7d8d8ab0999a31be9d2b34609
liaozhimingandy@qq.com <liaozhimingandy@qq.com>
2025-01-04 09:39:57 +0800

    feat(core): 添加示例
    1.使用pdm管理依赖

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -11,3 +12,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/api/chat")
+async def chat():
+    return "Hello World"
